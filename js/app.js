b971e3a75d41423a7feae9f2a9df0ead3e6362ab
Leo <hamlap6@gmail.com>
2020-04-01 16:58:03 -0700

    fixed timing issuees with targets dropping below 0

--- js/app.js
+++ js/app.js
@@ -1,77 +1,235 @@
 $(() => {
 
-
-// on opening, have modal that displays intro
+// VARIABLES
     const $introModal = $('#intro-modal');
     const $closeIntro = $('#close-intro');
-
-    $closeIntro.on('click', () => {
-        $introModal.hide();
-        createTargets();
-    });
-
-
-// timer on top
-    // simply counts down from variable time, which by default
-    // will be a set amount of time, but can be adjusted at bottom
     let time = 30;
     const $roundTime = $('#time').text(time);
     let timer;
+    const $score = $('#score');
+    const $missCounter = $('#miss-counter');
+    const $remainingTargets = $('#remaining-targets');
+    const $round = $('#round');
+    let score = 0;
+    let missCounter = 0;
+    let remainingTargets = {
+        val: 10,
+        min: 0,
+        removeTarget() {
+            if(this.val > 0){
+                this.val --;
+            } else {
+                this.val = 0;
+            }
+            return this.val;
+        }
+    }
+    let round = 1;
+    const pestArray = [
+        "https://cdn.pixabay.com/photo/2013/07/13/10/26/snail-157212_1280.png", "https://cdn.pixabay.com/photo/2012/04/02/14/24/hungry-worm-24634_1280.png", "https://cdn.pixabay.com/photo/2013/07/13/10/15/cockroach-156887_1280.png"  
+    ]
+    const $gamezone = $('#play-area');
+    const $gameOverModal = $('#game-over-modal');
+    const $closeGameOver = $('#close-game-over');
+    const targetDuration = 1;
+    const interval = 1;
 
-    // round timer function
+    let ranOutOfTime = false;
+// FUNCTIONS
     const countDownTime = () => {
         timer = setInterval(function() {
             time --;
-            console.log(time);
+            // console.log(time);
             
             $roundTime.text(time)
             if(time <= 0){
                 $roundTime.css('font-size', '23px');
                 $roundTime.text('TIME IS UP');
+                ranOutOfTime = true;
                 // remove event listeners within play zone once time is up  
                 clearInterval(timer);
             }
         }, 1000);
     }
-    
-    // starts timer once intro is closed
-    $closeIntro.on('click', countDownTime);
 
     const resetTimer = () => {
         time = 30;
         countDownTime();
     }
 
-
-// info area
-    const $score = $('#score');
-    const $missCounter = $('#miss-counter');
-    const $remainingTargets = $('#remaining-targets');
-    const $round = $('#round');
-
-    let score = 0;
-    let missCounter = 0;
-    let remainingTargets = 5;
-    let round = 1;
-    // score which will update continuously upon clicks
-    // miss clicked which will track expired targets and if you click on body
-    // targets left will be default 20 targets, but can be set in bottom
-    // round count will be default 5, but can be adjusted
-    
     const updateInfo = () => {
         $score.text(score);
         $missCounter.text(missCounter);
-        $remainingTargets.text(remainingTargets);
+        $remainingTargets.text(remainingTargets.val);
         $round.text(round);
     }
+
+    //  randomizes  x and y coordinates of target
+    const randomLocationX = () => {
+        // for both x and y, the first target locatin is always at origin because .target.width is undefined/nan at the first one becasue it has not been appended
+        // if all targets on screen are clicked and there are stilll more targets, next one will always be at origin
+        // this is solved by creating a div in html with target class and setting display none;
+        //  or by setting img size to a standart 40 px, can randomize.
+        // const width = Math.random() * ($gamezone.width() - 40);
+        const width = Math.random() * ($gamezone.width() - $('.target').width());
+        return width;
+    }
+    const randomLocationY = () => {
+        // const height = Math.random() * ($gamezone.height() - 40);
+        const height = Math.random() * ($gamezone.height() - $('.target').height());
+        return height;
+    }
+
+     // makes targets unclickable after time is up or miss too many
+     const stopTargets = () => {
+        $('.target').off();
+    }
+
+    // game over modal
+    const gameOver = () => {
+        clearInterval(timer);
+        $gameOverModal.css('display', 'block');
+        // repeats for 4 seconds
+        console.log('hi');
+        
+    }
+
+    // removes target on click
+    const clickTarget = event => {
+        const target = event.currentTarget;
+        // clicking on image will remove the div and add a point to score while decrementing target left
+        // increment score and decrement targets left
+        score += 100;
+        remainingTargets.removeTarget();
+        updateInfo();
+        target.remove();
+        // if(remainingTargets.val === 0){
+        //     clearInterval(timer);
+        // }
+    }
+
+
+// GAME LOGIC
+
+let generateTarget;
+
+// Logic for generating targets
+const createTargets = () => {
+    let i = remainingTargets.val;
+    generateTarget = setInterval(function() {
+        let x = Math.floor(Math.random() * (pestArray.length));
+        let randomName = Math.floor(Math.random() * 20000);
+        const target = $('<div>').addClass(`target key-${randomName}`);
+        target.css('background-image', `url(${pestArray[x]}`);
+        target.css({
+            'top': `${randomLocationY()}px`,
+            'left': `${randomLocationX()}px`
+        });
+
+        target.on('click', clickTarget);
+
+        const targetDur = () => {
+            return new Promise(function (resolve, reject) {
+                setTimeout(resolve, (targetDuration * 1000));
+            }).then((data) => {
+                if ($(`.key-${randomName}`).length > 0) {
+                    target.remove();
+                    missCounter++;
+                    remainingTargets.removeTarget();
+                    // if a target is missed and then the next one is not missed, it is counted as if missed
+                    console.log(remainingTargets.val);
+                    updateInfo();
+                }
+            });
+        }
+        targetDur();
+
+        
+        $gamezone.append(target);
+        
+        i --;
+
+
+        if(i <= 0 || missCounter >= 3){
+            // if($roundTime.text() === "TIME IS UP" || missCounter >= 3){
+            //     clearInterval(generateTarget);
+            //     stopTargets();
+            //     console.log('2');
+                
+            //     // gameOver();
+            // }
+            clearInterval(generateTarget);
+            gameOver();
+            // stopTargets();
+            // if(ranOutOfTime === true || missCounter >= 3){
+            //     // clearInterval(generateTarget);
+            //     clearInterval(timer);
+            //     stopTargets();
+            //     ranOutOfTime = false;
+            //     // gameOver();
+            // }
+            // console.log("1");
+            
+        } 
+    
+    }, (interval*1000));
+}
+
+
+// EVENT HANDLERS
+$closeIntro.on('click', () => {
+    $introModal.hide();
+    createTargets();
+});
+// starts timer once intro is closed
+$closeIntro.on('click', countDownTime);
+
+$closeGameOver.on('click', () => {
+    $gameOverModal.hide();
+})
+
+$('#rules').on('click', resetTimer);
+
+
+
+
+
+
+
+// on opening, have modal that displays intro
+    
+
+   
+
+
+// timer on top
+    // simply counts down from variable time, which by default
+    // will be a set amount of time, but can be adjusted at bottom
+    
+
+    // round timer function
+    
+    
+    
+    
+
+    
+
+
+// info area
+   
+    // score which will update continuously upon clicks
+    // miss clicked whi ch will track expired targets and if you click on body
+    // targets left will be default 20 targets, but can be set in bottom
+    // round count will be default 5, but can be adjusted
+    
+   
 
     
 // game area
     // 2 different arrays to pick from, of good and bad
     // maybe 1 array, with penalty being array index >= 3;
-    const pestArray = [
-        "https://cdn.pixabay.com/photo/2013/07/13/10/26/snail-157212_1280.png", "https://cdn.pixabay.com/photo/2012/04/02/14/24/hungry-worm-24634_1280.png", "https://cdn.pixabay.com/photo/2013/07/13/10/15/cockroach-156887_1280.png"  
-    ]
+    
 
     // const dontClickArray = [
     //     "https://cdn.pixabay.com/photo/2014/04/03/09/58/eggplant-309459_1280.png", "https://cdn.pixabay.com/photo/2012/04/18/20/21/strawberry-37781_1280.png", "https://cdn.pixabay.com/photo/2013/07/12/18/19/tomato-153272_1280.png"
@@ -82,101 +240,14 @@ $(() => {
     //     "https://cdn.pixabay.com/photo/2013/07/13/10/26/snail-157212_1280.png", "https://cdn.pixabay.com/photo/2012/04/02/14/24/hungry-worm-24634_1280.png", "https://cdn.pixabay.com/photo/2013/07/13/10/15/cockroach-156887_1280.png", "https://cdn.pixabay.com/photo/2014/04/03/09/58/eggplant-309459_1280.png", "https://cdn.pixabay.com/photo/2012/04/18/20/21/strawberry-37781_1280.png", "https://cdn.pixabay.com/photo/2013/07/12/18/19/tomato-153272_1280.png"
     // ]
 
-    const $gamezone = $('#play-area');
+    
 
 
-//  randomizes  x and y coordinates of target
-    const randomLocationX = () => {
-        // for both x and y, the first target locatin is always at origin because .target.width is undefined/nan at the first one becasue it has not been appended
-        const width = Math.random() * ($gamezone.width() - $('.target').width());
-        return width;
-    }
-    const randomLocationY = () => {
-        const height = Math.random() * ($gamezone.height() - $('.target').height());
-        return height;
-    }
 
-// Logic for generating targets
-    const createTargets = () => {
-        let i = remainingTargets;
-        const targetInterval = setInterval(function() {
-            let x = Math.floor(Math.random() * (pestArray.length));
-            let randomName = Math.floor(Math.random() * 20000);
-            const target = $('<div>').addClass(`target key-${randomName}`);
-            target.css('background-image', `url(${pestArray[x]}`);
-            target.css({
-                'top': `${randomLocationY()}px`,
-                'left': `${randomLocationX()}px`
-            });
 
-            target.on('click', clickTarget);
 
-            const targetDur = () => {
-                return new Promise(function (resolve, reject) {
-                    setTimeout(resolve, (targetDuration * 1000));
-                }).then((data) => {
-                    if ($(`.key-${randomName}`).length > 0) {
-                        target.remove();
-                        missCounter++;
-                        remainingTargets--;
-                        updateInfo();
-                    }
-                });
-            }
-            targetDur()
-
-            
-            $gamezone.append(target);
-            
-            i --;
-            
-            if(i <= 0 || $roundTime.text() === "TIME IS UP") {
-                clearInterval(targetInterval);
-                clearInterval(timer);
-                stopTargets();
-                    // if($roundTime.text() === "TIME IS UP"){
-                // }
-                // gameOver();
-            }
-        }, (interval*1000));
-    }
 
    
-    // makes targets unclickable after time is up or miss too many
-    const stopTargets = () => {
-        $('.target').off();
-    }
-
-    const gameOver = () => {
-        clearInterval(timer);
-        $gameOverModal.css('display', 'block');
-        // repeats for 4 seconds
-        console.log('hi');
-        
-    }
-    
-    // game over modal
-    const $gameOverModal = $('#game-over-modal');
-    const $closeGameOver = $('#close-game-over');
-    $closeGameOver.on('click', () => {
-        $gameOverModal.hide();
-    })
-
-
-    // removes target on click
-    const clickTarget = event => {
-        const target = event.currentTarget;
-        // clicking on image will remove the div and add a point to score while decrementing target left
-        // increment score and decrement targets left
-        score += 100;
-        remainingTargets --;
-        updateInfo();
-        target.remove();
-        if(remainingTargets === 0){
-            clearInterval(timer);
-        }
-    }
-
     
     
     // math.random to randomly generate position x and position y
@@ -189,18 +260,18 @@ $(() => {
     
     
     // option area
-    const interval = 1;  //could change to ms
+      //could change to ms
     // rules will be a button that opens modal that explains the rules of game
     // num of popups will take input that adjusts target left in info aka max targets/round
     // popup size will take input that adjusts size of divs -> images
     // round timer will adjsut time counter at top, which stays same at each round
     // target duration affects how long target lasts
-    const targetDuration = 3;
     
     
     
     
-    $('#rules').on('click', resetTimer)
+    
+    
     // createTargets();
 
 })
